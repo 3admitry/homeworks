b17ca9aab106090632136650a08c93e43401bfc4
3admitry <3a.dmitriy@gmail.com>
2022-04-21 01:11:50 +0300

    super-input -button - checkbox

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -63,6 +63,8 @@ function HW4() {
                     some text {/*// этот текст попадёт в children*/}
                 </SuperCheckbox>
 
+
+
                 {/*// onChange тоже должен работать*/}
                 <SuperCheckbox checked={checked} onChange={testOnChange}/>
             </div>
